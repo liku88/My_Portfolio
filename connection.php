3bbad0258dee4c5edeb0256e3a40526a3f2d8a9d
Mangaldeep Das <rockonliku88@gmail.com>
2020-07-08 23:43:45 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+$username = 'id14295534_root';
+$password = 'ima8(D})tmbHo~T>';
+$server = 'localhost';
+$db = 'id14295534_mangaldeepdb';
+
+$con = mysqli_connect($server,$username,$password,$db);
+
+
+
+?>
