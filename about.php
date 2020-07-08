4e272204f4268c931e8df9c73d45b93b912f2b26
Mangaldeep Das <rockonliku88@gmail.com>
2020-07-08 23:43:19 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Mangaldeep Das | About</title>
+    <link
+      rel="stylesheet"
+      href="./fontawesome-free-5.12.1-web/css/all.min.css"
+    />
+    <!-- css -->
+        <link rel="stylesheet" media="screen" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <link rel="stylesheet" href="./css/styles.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+     <link rel="shorcut icon" type="image/png" href="favicon.png">
+  </head>
+
+  <body>
+    <!-- navbar -->
+    <nav class="nav" id="nav">
+      <div class="nav-center">
+        <!-- nav header -->
+        <div class="nav-header">
+          <a class="navbar-brand animation-1 sign" href="#"><span class="animation-2" >M</span>
+      <span class="animation-2">a</span>
+      <span  class="animation-2">n</span>
+      <span  class="animation-2">g</span>
+      <span  class="animation-2">a</span>
+      <span  class="animation-2">l</span>
+      <span  class="animation-2">d</span>
+      <span  class="animation-2">e</span>
+      <span  class="animation-2">e</span>
+      <span  class="animation-2">P</span>
+      
+      </a>
+          <button class="nav-btn" id="nav-btn">
+            <i class="fas fa-bars"></i>
+          </button>
+        </div>
+        <!-- nav-links -->
+        <ul class="nav-links">
+        
+       <li><a class="tryna1" href="index.php">
+           Home</a></li>
+        
+         
+
+         <li><a  class="tryna2" href="about.php">
+         <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+         About</a></li>
+          
+
+         <li><a  class="tryna6" href="projects.php"><span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+            Projects</a></li>
+          
+
+        <li><a  class="tryna7" href="contact.php"><span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+            Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+    <!-- end of navbar -->
+
+    <!-- sidebar -->
+    <aside class="sidebar" id="sidebar">
+      <div>
+        <button class="close-btn" id="close-btn">
+          <i class="fas fa-times"></i>
+        </button>
+        <!-- nav-links -->
+        <ul class="sidebar-links">
+          <li>
+            <a href="index.php">home</a>
+          </li>
+          <li>
+            <a href="about.php" onclick="closeSidebar()">about</a>
+          </li>
+          <li>
+            <a href="projects.php" onclick="closeSidebar()">projects</a>
+          </li>
+          <li>
+            <a href="contact.php" onclick="closeSidebar()">contact</a>
+          </li>
+        </ul>
+      </div>
+    </aside>
+    <!-- end of sidebar -->
+
+   
+
+    <section class="section single-page">
+      <!-- section title -->
+      <div class="section-title">
+        <h1>about me</h1>
+        <div class="underline"></div>
+      </div>
+      <!--end of section title -->
+      <div class="section-center page-info">
+        <!-- about page -->
+        <p>
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis similique suscipit, iusto, obcaecati nam illo, culpa est quidem soluta dolorem maxime consectetur? Asperiores voluptate minus dolorem ab doloremque, perferendis dolores explicabo sapiente quisquam similique, ratione ea, sint! Neque illum repudiandae est quae facere atque quos omnis voluptatibus, adipisci voluptate laborum pariatur sit in a beatae rerum possimus distinctio rem, minima at recusandae vel laboriosam officiis vero, deserunt! Repellat officia a voluptas, quisquam minima doloribus at aliquid praesentium quibusdam nihil necessitatibus facere reprehenderit rem adipisci odio aliquam illo vel, asperiores, tempore voluptatibus eos dicta similique temporibus exercitationem molestiae! Labore. 
+        </p>
+      </div>
+    </section>
+  
+
+    <!-- footer -->
+    <footer class="footer">
+      <p>&copy; <span id="date"></span> Mangaldeep Das. all rights reserved</p>
+    </footer>
+    
+    <script src="js/app.js"></script>
+  </body>
+</html>
